59f36e816e693034e246b6fb9cdfa17346ea513b
Janitha Dissanayaka <janithadissnayakaa@gmail.com>
2025-02-12 13:24:36 +0530

    Modify the Navebar and create a New component for Details

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import logo from "../assets/assets/Logo.png";
 import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaEnvelope } from "react-icons/fa";
 
 
 const Navbar=()=>{
- return   <nav className="bg-blue-900 mb-20 flex items-center justify-between py-6 h-[75px]">
+ return   <nav className="mb-20 flex items-center justify-between py-6 h-[75px]">
  <div className="flex flex-shrink-0 items-center">
-   <img src={logo} alt="logo" width="75px" height="75px" />
+   <img src={logo} alt="logo" width="100px" height="100px" />
  </div>
- <div width="50px" height="50px">
-    <FaLinkedin className="text-white text-[30px]"/>
+ <div className="m-8 flex items-center justify-center gap-4 text-white text-2xl">
+    <a href="/"><FaLinkedin  /></a>
+    <a href=""><FaGithub /></a>
+    <a href=""><FaEnvelope/></a>
  </div>
 </nav>
 };
