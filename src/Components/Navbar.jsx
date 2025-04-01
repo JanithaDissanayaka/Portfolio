984ee24f874abab4d8fb81a61c569255681280f3
Janitha Dissanayaka <155894806+JanithaDissanayaka@users.noreply.github.com>
2025-04-01 16:25:32 +0530

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "../assets/logo.png";
+
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { FaEnvelope } from "react-icons/fa";
@@ -7,9 +7,7 @@ import { FaEnvelope } from "react-icons/fa";
 
 const Navbar=()=>{
  return   <nav className="mb-20 flex items-center justify-between py-6 h-[75px]">
- <div className="flex flex-shrink-0 items-center">
-   <img src={logo} alt="logo" width="100px" height="100px" />
- </div>
+
  <div className="m-8 flex items-center justify-center gap-4 text-white text-2xl">
     <a href="/"><FaLinkedin  /></a>
     <a href=""><FaGithub /></a>
