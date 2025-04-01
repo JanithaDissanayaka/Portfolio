33c2dd177a0de081d49055a199a67b5440ba1a9f
Janitha Dissanayaka <155894806+JanithaDissanayaka@users.noreply.github.com>
2025-04-01 16:40:49 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Portfolio/", // Change "Portfolio" to your repo name
   plugins: [react()],
+  base: "/",
 });
 
